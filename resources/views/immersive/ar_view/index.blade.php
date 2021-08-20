eef18ceb7ec45323e07e8766a25c5d1a81100725
ZulZayn <zulwaqarzain96@gmail.com>
2021-08-20 19:04:12 +0800

    ar

--- resources/views/immersive/ar_view/index.blade.php
+++ resources/views/immersive/ar_view/index.blade.php
@@ -1,18 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>AR Scene</title>
-    
-    <!-- import the webpage's stylesheet -->
-    
-    <!-- import the webpage's javascript file -->
-    <script src="https://aframe.io/releases/1.0.4/aframe.min.js"></script>
-    <script src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar-nft.js"></script>
-    <script src="https://unpkg.com/aframe-extras.animation-mixer@3.4.0/dist/aframe-extras.animation-mixer.js"></script>
+<html>
 
     <style>
       .btn-primary {
@@ -38,28 +25,31 @@
         background-color: #ef2d5e;
       }
     </style>
-  </head>  
-  <body style="margin : 0px; overflow: hidden;">
-    <a-scene embedded arjs>
-      <a-assets>
-        <a-asset-item id="3dmodel" 
-          src="{{ URL::to(''.$modelSrc->file_path.'') }}"></a-asset-item>
-        </a-assets>
-      
-      <div>
-        <button class="change-size btn-primary" style="left: 0; top: 0" addvalue="-0.05" cursor-listener>- Reduce size</button>
-        <button class="change-size btn-primary" style="left: 100px; top: 0" addvalue="0.05" cursor-listener>+ Add size</button>
-      </div>
 
-      <a-marker type="pattern" url="{{ URL::to('assets/ar-markers/pattern-AR-marker.patt') }}">
-        <a-entity id="gltfModel" gltf-model="#3dmodel" 
-          position="0 0 0" 
-          animation-mixer></a-entity>
-      </a-marker>
-      <a-entity camera></a-entity>
-    </a-scene>
+    <script src="https://aframe.io/releases/1.0.0/aframe.min.js"></script>
+    <!-- we import arjs version without NFT but with marker + location based support -->
+    <script src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar.js"></script>
 
-    <script>
+    <body style="margin : 0px; overflow: hidden;">
+        <a-scene embedded arjs>
+
+          <div>
+            <button class="change-size btn-primary" style="left: 0; top: 0" addvalue="-0.05" cursor-listener>- Reduce size</button>
+            <button class="change-size btn-primary" style="left: 100px; top: 0" addvalue="0.05" cursor-listener>+ Add size</button>
+          </div>
+
+        <a-marker type="pattern" url="{{ asset('assets/ar-markers/pattern-AR-marker.patt') }}">
+            <a-entity id="gltfModel"
+            position="0 -1 0"
+            scale="0.05 0.05 0.05"
+            gltf-model="{{ URL::to(''.$modelSrc->file_path.'') }}"
+            >
+          </a-entity>
+        </a-marker>
+        <a-entity camera></a-entity>
+        </a-scene>
+
+         <script>
       var buttons = document.querySelectorAll('.change-size');
       
       for(var i = 0; i < buttons.length; i++) {
@@ -77,5 +67,6 @@
         });
       }
     </script>
-  </body>
-</html>
+    </body>
+</html> 
+
